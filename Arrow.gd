9fb350346b407adb9608d83c2be0bcd73a0ef380
anon <>
2022-01-21 15:28:49 -0800

    add psuedo 3d effect for arrows

--- Arrow.gd
+++ Arrow.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
+onready var arrow_sprite = get_node("ArrowSprite")
 
-var SPEED = 200
-# Declare member variables here. Examples:
-var yvel = 2
-var ygrav = 2
-
+var SPEED = 300
+var yvel = 0
+var zvel = 0
+var zgrav = 2
+var arrow_init_y = -30
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +14,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_and_slide(Vector2(SPEED,0))
+	arrow_sprite.position.y += zvel
+	zvel += zgrav * delta
+	if(arrow_sprite.position.y > 0):
+		queue_free()
+	move_and_slide(Vector2(SPEED,yvel))
 	if get_slide_count()==1:
 		get_slide_collision(0).get_collider().queue_free()
 		queue_free()
+		
+func set_arrow_target(x,y):
+	# where should the arrow land, relative to the start position?
+	
+	# let's assume that time scales linearly with distance.
+	var airtime = abs(Vector2(x,y).length() / SPEED)
+	# let's start by solving for y first.
+	# i do not know why i have to divide by ten here??
+	zvel = (0.3 + -(0.5 * zgrav * airtime * airtime)) / airtime
+	yvel = (y / airtime)
+	
--- Player.gd
+++ Player.gd
@@ -32,4 +32,7 @@ func _input(event):
 		var instance = arrow.instance()
 		get_parent().add_child(instance)
 		instance.global_position = global_position
+		var target_x = event.position.x + get_node("../Camera").global_position.x
+		var diff_x = target_x + -global_position.x + -450 # cam bias
+		instance.set_arrow_target(diff_x, event.position.y - global_position.y)
 		
