38450cc45e10f81e0ea11c693f815065f2879a50
anon <>
2022-01-23 00:36:38 -0800

    can go from title through game to inventory

--- Scripts/CameraFollow.gd
+++ Scripts/CameraFollow.gd
@@ -2,6 +2,7 @@ extends Camera2D
 
 var PROGRESS_LINE = 300
 var frozen = false
+var endgame = false
 
 var targ = 0
 # Called when the node enters the scene tree for the first time.
@@ -13,9 +14,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position.x = lerp(global_position.x, targ, 3 * delta)
+	if(endgame):
+		if(get_node("../RightBarrier")):
+			get_node("../RightBarrier").queue_free()
+		if(get_node("../Player").global_position.x + 450 > global_position.x):
+			get_parent().finish()
+			
 	if(frozen):
 		return
 	if(get_node("../LeftBarrier").global_position.x < get_node("../Player").global_position.x - PROGRESS_LINE):
 		get_node("../LeftBarrier").global_position.x = get_node("../Player").global_position.x - PROGRESS_LINE
 		get_node("../RightBarrier").global_position.x = get_node("../Player").global_position.x - PROGRESS_LINE + 900
 		targ = (450 - PROGRESS_LINE) + get_node("../Player").global_position.x
+
+			
+		
