be566359f211d0c5dde2592a56cea3454c979beb
anon <>
2022-01-22 17:15:27 -0800

    improve character rig

--- Player.gd
+++ Player.gd
@@ -33,20 +33,28 @@ func _physics_process(delta):
 		move_and_slide(SPEED * raw_input)
 	
 	var target = Vector2(0,10000)
+	
 	if(aimer_active):
 		target = Vector2(-450 + get_node("../Camera").global_position.x + get_viewport().get_mouse_position().x, get_viewport().get_mouse_position().y)
 		aim_time += delta
 		if(aim_time > AIM_LIMIT):
 			aim_time = AIM_LIMIT
-		
+		rigAim(getAimerPosition())
 	else:
 		aim_time = 0
 		get_node("../Aimer").rotation_degrees = 0
+		
 	get_node("../Aimer").global_position = target
 	var aimscale = pow((AIM_MAX - aim_time) / AIM_MAX,0.8)
 	get_node("../Aimer").global_scale = Vector2(aimscale,aimscale)
 	get_node("../Aimer").rotation_degrees += AIM_CURS_ROT_SPEED * delta
-	
+
+func getAimerPosition():
+	var target_x = get_viewport().get_mouse_position().x + get_node("../Camera").global_position.x
+	var diff_x = target_x + -global_position.x + -450 # cam bias
+	var diff_y = get_viewport().get_mouse_position().y - global_position.y
+	return Vector2(diff_x,diff_y)
+
 func _input(event):
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
@@ -55,7 +63,10 @@ func _input(event):
 		var instance = arrow.instance()
 		get_parent().add_child(instance)
 		instance.global_position = global_position
-		var target_x = event.position.x + get_node("../Camera").global_position.x
-		var diff_x = target_x + -global_position.x + -450 # cam bias
-		instance.set_arrow_target(diff_x, event.position.y - global_position.y)
+		instance.set_arrow_target(getAimerPosition()[0],getAimerPosition()[1]) 
 		aimer_active = false
+		
+func rigAim(vec):
+	# offset + rad to deg
+	get_node("CharacterRig/Pelvis/BoneTorso/Torso/BoneUpperRightArm").rotation_degrees = -50 + 57 * atan2(vec.y,vec.x)
+	get_node("CharacterRig/Pelvis/BoneTorso/Torso/BoneUpperLeftArm").rotation_degrees = -50 + 57 * atan2(vec.y,vec.x)
