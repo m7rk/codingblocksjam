afe089c0615aae43e3fbf33b1df1b9a53b6e4aac
anon <>
2022-01-22 23:31:21 -0800

    Add listeners for boss encounter

--- Scenes/DungeonManager.gd
+++ Scenes/DungeonManager.gd
@@ -9,12 +9,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Tween").interpolate_property(get_node("Camera/FrontSprite"), "modulate", Color(1,1,1,1), Color(0,0,0,0), 3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-	get_node("Tween").interpolate_property(get_node("Music"), "volume_db", -80, 0, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	get_node("Tween").interpolate_property(get_node("Music/Main"), "volume_db", -80, 0, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	get_node("Tween").start()
 
 func finish():
 	get_node("Tween").interpolate_property(get_node("Camera/FrontSprite"), "modulate", Color(0,0,0,0), Color(1,1,1,1), 3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-	get_node("Tween").interpolate_property(get_node("Music"), "volume_db", 0, -80, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	get_node("Tween").interpolate_property(get_node("Music/Main"), "volume_db", 0, -80, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	get_node("Tween").start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
