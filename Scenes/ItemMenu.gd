acf1279d013b220e16b76d00bf8d7544bed4d53b
anon <>
2022-01-24 05:18:32 -0800

    add transisiton codes to item mnu

---
+++ Scenes/ItemMenu.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var exiting = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	AppState.level_to_load += 1
+	get_node("Tween").interpolate_property(get_node("Transitioner"), "modulate", Color(0,0,0,0), Color(1,1,1,1), 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	get_node("Tween").interpolate_property(get_node("AudioStreamPlayer"), "volume_db", 0, -80, 3, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+
+func _on_Tween_tween_all_completed():
+	get_tree().change_scene("res://Scenes/Dungeon.tscn")
+
+func _on_Button_pressed():
+	exiting = true
+	get_node("Tween").start()
+
+func _process(delta):
+	if(!exiting):
+		get_node("Transitioner").modulate = lerp(get_node("Transitioner").modulate,Color(0,0,0,0), delta)
