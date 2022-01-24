8d7c933887b739489ce2f33f2b838df4ecd7fe4c
anon <>
2022-01-24 00:34:17 -0800

    more test anims

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,6 +8,7 @@ var AIM_SPEED = 60
 # var b = "text"
 var aimer_active = false
 var aim_time = 0
+var draw_time = 0
 var AIM_MAX = 2.5
 var AIM_LIMIT = 2.0
 var AIM_CURS_ROT_SPEED = 90
@@ -39,9 +40,9 @@ func _physics_process(delta):
 		raw_input = Vector2(0,1)
 	
 	if(raw_input != Vector2(0,0)):
-		get_node("CharacterRig/Animator").play("Walk")
+		get_node("CharacterRig/LAnimator").play("Walk")
 	else:
-		get_node("CharacterRig/Animator").play("Idle")
+		get_node("CharacterRig/LAnimator").play("Idle")
 	
 	if(aimer_active):
 		move_and_slide(AIM_SPEED * raw_input)
@@ -49,6 +50,11 @@ func _physics_process(delta):
 		move_and_slide(SPEED * raw_input)
 	
 	var target = Vector2(0,10000)
+	
+	if(draw_time >= 0):
+		arrow_node.visible = false
+		draw_time -= delta
+		return
 	
 	if(aimer_active):
 		target = Vector2(-450 + get_node("../Camera").global_position.x + get_viewport().get_mouse_position().x, get_viewport().get_mouse_position().y)
@@ -94,10 +100,15 @@ func isReversed():
 	return sign(get_node("CharacterRig").scale.x) == -1
 	
 func _input(event):
+	if(draw_time > 0):
+		return
 	# Mouse in viewport coordinates.
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		aimer_active = true
+		get_node("CharacterRig/UAnimator").stop()
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and !event.pressed:
+		if(aimer_active != true):
+			return
 		var instance = arrow.instance()
 		get_parent().add_child(instance)
 		# little nudge to make arrow heads land better
@@ -105,6 +116,8 @@ func _input(event):
 		var rand = addAimRandomness(40 * getAimScale())
 		instance.set_arrow_target(getAimerPosition()[0] + rand.x, getAimerPosition()[1] + rand.y) 
 		aimer_active = false
+		draw_time = 2
+		get_node("CharacterRig/UAnimator").play("Draw")
 		
 func rigAim(vec):
 	# properly animate the left arm
