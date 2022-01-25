128e6a85ddeee0d654c27dc534d808bec79e6666
anon <>
2022-01-24 17:48:51 -0800

    untested boss revamps

--- Scripts/SwordBoss.gd
+++ Scripts/SwordBoss.gd
@@ -7,10 +7,8 @@ extends KinematicBody2D
 
 var phase = "TAUNTING"
 var active = false
-# state = taunting 
-# state = teleporting
-# state = attacking
-# state = injured
+var HP = 3
+var transition_time = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,19 +17,36 @@ func _ready():
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if(active):
 		if(phase == "TAUNTING"):
-			pass
-
+			get_node("Animation").play("Taunt")
+			transition_time -=  delta
+			if(transition_time < 0):
+				phase == "ATTACKING"
+		if(phase == "ATTACKING"):
+			get_node("Animation").play("Attack")
+			move_and_slide(Vector2(-200,0))
+			if(global_position.x < -500 + get_node("../../Player").x):
+				phase = "ATTACKING"
+				global_position.x = get_node("../../Player").x + 500
+			
+			
 func onHit(bonus):
-	get_node("../../Music").fadeOut()
-	if(bonus):
-		get_node("../../Peter").killBossBonus()
-	get_node("CollisionShape2D").queue_free()
-	get_node("Tween").start()
-	phase = "DEAD"
-	yield(get_tree().create_timer(2.0), "timeout")
-	get_node("../../Camera").frozen = false
-	get_node("../../Music").playSong("Main")
-	queue_free()
+	if(phase == "ATTACKING"):
+		HP -= 1
+		if(HP == 0):
+			get_node("../../Music").fadeOut()
+			if(bonus):
+				get_node("../../Peter").killBossBonus()
+			get_node("CollisionShape2D").queue_free()
+			get_node("Tween").start()
+			phase = "DEAD"
+			yield(get_tree().create_timer(2.0), "timeout")
+			get_node("../../Camera").frozen = false
+			get_node("../../Music").playSong("Main")
+			queue_free()
+	else:
+		get_node("Animation").play("BlockHi")
+		transition_time = 5
+		
