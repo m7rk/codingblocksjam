e17a2bda47b7c333125d3289be258f32c14e85e3
Anon <>
2023-03-01 21:13:52 -0800

    head bone

--- Scripts/SkeletonHead.gd
+++ Scripts/SkeletonHead.gd
@@ -4,7 +4,7 @@ var SPEED = 80
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_node("Tween").interpolate_property(self, "scale", Vector2(1,1), Vector2(0,0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	get_node("Tween").interpolate_property(self, "scale", Vector2(0.8,0.8), Vector2(0,0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
